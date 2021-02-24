ba385f3ac7ab5a73e0f67d0b8c21c69fd66b9bba
InetaVei <74532995+InetaVei@users.noreply.github.com>
2021-02-24 22:47:50 +0200

    Namu darbai

---
+++ index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        /* NAMU DARBAI
+        1. Pailiustruoti operacijas (array_sum, min, max) su:
+            1.1. vienmaciu masyvu
+            1.2. asociatyviu masyvu
+            1.3. daugiamaciu masyvu
+        */
+
+    // vienamtis masyvas
+    $arr1 = array(4, 1, 7, 7, 4, 5);
+    print_r($arr1);
+    
+    // maziausias
+    print("<br>");
+    print "sio masyvo maziausias indeksas yra :" . "\n" . min($arr1);
+
+    // didziausias
+    print("<br>");
+    print "sio masyvo didziausias indeksas yra :" . "\n" . max($arr1);
+
+    //suma
+    print("<br>");
+    print "sio masyvo suma = " . array_sum($arr1) . "\n";
+    print("<br>");
+
+
+    // // ASOCIATYVUS MASYVAS
+    print("<br>");
+    $array_family = array(
+        "mom" => "30",
+        "dad" => "35",
+        "kid1" => "5",
+        "kid2" => "7"
+    );
+    print_r($array_family);
+
+    // jauniausias
+    print("<br>");
+    print "jauniausias seimoje :". "\n" . min($array_family);
+
+    // vyriausias
+    print("<br>");
+    print "jauniausias seimoje :". "\n" . max($array_family);
+
+    // amziaus suma
+    print("<br>");
+    print "seimos bendras amzius :" . "\n" . array_sum($array_family) . "\n";
+
+
+    // // DAUGIAMATIS MASYVAS
+
+
+
+    /* 2. Sukurkite asociatyvųjį masyvą, kuris reprezentuotų žmogų ir jo svorį ( “vardas1” => “svoris1”, “vardas2” => “svoris2” … ) ir:
+            2.1. Raskite lengviausią žmogų (minimumas);
+            2.2. Sunkiausią (maximumas);
+            2.3. Susumuokite visų žmonių svorį ir nustatykite ar jie gali kilti liftu (500kg ar kita riba);
+            2.4. * Atspausdinkite surikiuotus žmones (pagal svorį);
+*/
+    print("<br>");
+    $arr4 = array(
+        "Agne" => "59",
+        "Julius" => "80",
+        "Vilius" => "78",
+        "Mila" => "60"
+    );
+    
+    // Sukurkite asociatyvųjį masyvą, kuris reprezentuotų žmogų ir jo svorį
+    print("<br>");
+    print_r($arr4);
+
+    // Raskite lengviausią žmogų (minimumas);
+    print("<br>");
+    print "Lengviausias is visu :" . "\n" . min($arr4) . "\n";   // NEPAVYKSTA NURODYTI VARDO
+    
+    // Sunkiausią (maximumas);
+    print("<br>");
+    print "Sunkiausias is visu :" . "\n" . max($arr4) . "\n";    // NEPAVYKSTA NURODYTI VARDO
+
+    // Susumuokite visų žmonių svorį
+    print("<br>");
+    print "Visu zmoniu bendras svoris yra: " . "\n" . array_sum($arr4);
+
+    // nustatykite ar jie gali kilti liftu (500kg ar kita riba);
+    print("<br>");
+    if(array_sum($arr4) < 500) {
+        print "zmones gali kilti liftu, nes bendras svoris nevirsyja 500";
+    }
+
+    // Atspausdinkite surikiuotus žmones (pagal svorį);
+    print("<br>");
+    $arr5 = array("Agne" => "59", "Julius" => "80", "Vilius" => "78", "Mila" => "60");
+    asort($arr5);
+    foreach($arr5 as $key => $value) {
+        print ("$key = $value\n");
+    }
+?>
+</body>
+</html>
